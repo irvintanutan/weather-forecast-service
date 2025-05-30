68174f4237b1c59bca09753b55ad61c74cc2b7d1
Irvin Tanutan <vintot222@gmail.com>
2025-05-30 18:42:57 +0800

    Initial Commit

---
+++ src/main/java/com/example/weather_forecast_service/WeatherForecastServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather_forecast_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherForecastServiceApplication.class, args);
+	}
+
+}
